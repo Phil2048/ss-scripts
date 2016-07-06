5750e0304eb2590a999d149f9c46f8fa65399e11
yb3712590 <494888545@qq.com>
2016-07-06 17:47:47 +0800

    enable_tcp_fastopen

--- install_shadowsocks_libev.sh
+++ install_shadowsocks_libev.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+source /home/admin/ss_config.cfg
 yum install -y gcc automake autoconf libtool make build-essential autoconf libtool gcc
 yum install -y curl curl-devel zlib-devel openssl-devel perl perl-devel cpio expat-devel gettext-devel
 yum install -y git
+
 
 cd /opt
 git clone https://github.com/madeye/shadowsocks-libev.git
 cd /opt/shadowsocks-libev
 ./configure && make
 make install
+
+cd ${home}
+cp ./local.conf /etc/sysctl.d/local.conf
+sysctl --system
