0f6d6813b7a06140a64a8b53ceb02de65c22de58
yb3712590 <494888545@qq.com>
2017-02-15 17:55:34 +0800

    update install script

--- install_shadowsocks_libev.sh
+++ install_shadowsocks_libev.sh
@@ -2,13 +2,16 @@
 source /home/admin/ss_config.cfg
 yum install -y gcc automake autoconf libtool make build-essential autoconf libtool asciidoc xmlto
 yum install -y curl curl-devel zlib-devel openssl-devel perl perl-devel cpio expat-devel gettext-devel
-yum install -y git
+yum install -y git udns udns-devel libev-devel
+#mbedtls libsodium
 
 
 cd /opt
 rm -rf /opt/shadowsocks-libev
-git clone https://github.com/madeye/shadowsocks-libev.git
+git clone https://github.com/shadowsocks/shadowsocks-libev.git
 cd /opt/shadowsocks-libev
+git submodule update --init --recursive
+./autogen.sh
 ./configure && make
 make install
 
--- reset_ss.sh
+++ reset_ss.sh
@@ -7,7 +7,7 @@ mysql -u$user -p$password $database -N -e 'update user set active=0 where utype=
 mysql -u$user -p$password $database -N -e 'update user set active=0 where utype=0 and curdate()>exp'
 mysql -u$user -p$password $database -N -e 'update user set active=1 where utype=1 and upload+download<transfer_limit'
 mysql -u$user -p$password $database -N -e 'update user set active=1 where utype=0 and curdate()<=exp'
-mysql -u$user -p$password $database -N -e 'select port,ss_passwd from user where active=1 and uname not in ("397501170@qq.com")'|while read a b;
+mysql -u$user -p$password $database -N -e 'select port,ss_passwd from user where active=1'|while read a b;
 do 
 #test
 #echo "nohup /gopkg/bin/shadowsocks-server -p=\"$a\" -k=\"$b\" -m=\"table\" -t=60 >/dev/null & "
@@ -16,12 +16,12 @@ do
 #nohup /gopkg/bin/shadowsocks-server -p $a -k $b -m $method -t $timeout > /dev/null 2>&1 & 
 #
 #libev
-nohup ss-server -p $a -k $b -m $method -t $timeout -u -A --fast-open >/dev/null 2>&1 &
+nohup ss-server -p $a -k $b -m $method -t $timeout -u --fast-open >/dev/null 2>&1 &
 #python
 #nohup ssserver -p $a -k $b -m $method -t $timeout --fast-open >/dev/null 2>&1 &
 done
-mysql -u$user -p$password $database -N -e 'select port,ss_passwd from user where active=1 and uname in ("397501170@qq.com","likethisj")'|while read a b;
-do
-nohup ss-server -p $a -k $b -m $method -t $timeout -u --fast-open >/dev/null 2>&1 &
-done
+#mysql -u$user -p$password $database -N -e 'select port,ss_passwd from user where active=1 and uname in ("397501170@qq.com","likethisj")'|while read a b;
+#do
+#nohup ss-server -p $a -k $b -m $method -t $timeout -u -v --fast-open >/tmp/log_ss_6443_8443.log 2>&1 &
+#done
 
