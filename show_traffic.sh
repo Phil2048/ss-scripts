69f9b35efd7103b621570650d7527c500558c552
yb3712590 <494888545@qq.com>
2017-02-15 18:10:42 +0800

    add total traffic

--- show_traffic.sh
+++ show_traffic.sh
@@ -8,5 +8,6 @@ source /home/admin/ss_config.cfg
 #echo "-----------------------账户信息-----------------------"
 mysql -u$user -p$password $database -e "select dat_date,port,uname,concat(format(upload/1024/1024,0),'M') as upload,concat(format(download/1024/1024,0),'M') as download from $database.$hist_table order by port"
 echo 
-mysql -u$user -p$password $database -e "select port,ss_passwd,uname,if(utype=0,'时长','流量')as utype,concat(format(upload/1024/1024,0),'M') as upload,concat(format(download/1024/1024,0),'M') as download,concat(format(transfer_limit/1024/1024,0),'M') as 'limit',exp,if(active=1,'正常','欠费') as active from $database.$table order by port"
+mysql -u$user -p$password $database -e "select port,ss_passwd,uname,if(utype=0,'时长','流量')as utype,concat(format(upload/1024/1024,0),'M') as upload,concat(format(download/1024/1024,0),'M') as download,concat(format(transfer_limit/1024/1024,0),'M') as 'limit',exp,if(active=1,'正常','欠费') as active from $database.$table union all
+select 'all','---','---','---',concat(format(sum(upload)/1024/1024,0),'M'),concat(format(sum(download)/1024/1024,0),'M'),'---','---','---' from $database.$table order by cast(port as signed int);"
 date '+%Y%m%d %H:%M:%S'
