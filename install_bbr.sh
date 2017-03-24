3e5e7059da5d91de125306c344d4d09ae3d2ccef
yb3712590 <494888545@qq.com>
2017-03-24 10:31:59 +0800

    bbr things

---
+++ install_bbr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum update -y
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+
+yum --enablerepo=elrepo-kernel install -y kernel-ml
+
+echo 'net.core.default_qdisc = fq' >> /etc/sysctl.conf
+echo 'net.ipv4.tcp_congestion_control = bbr' >> /etc/sysctl.conf
+
+sysctl -p
