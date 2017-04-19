08e83bb1db04408343292e9d8f624fc824184f4a
yb3712590 <494888545@qq.com>
2017-04-19 10:49:25 +0800

    chmod@170419

--- install_LAMP.sh
+++ install_LAMP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install -y git httpd mariadb-server mariadb php php-mysql epel-release
-rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
-yum install -y phpmyadmin --enablerepo=remi
+#rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+#yum install -y phpmyadmin --enablerepo=remi
 service httpd start
 chkconfig httpd on
 service mariadb start
