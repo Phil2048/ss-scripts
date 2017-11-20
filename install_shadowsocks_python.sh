f310d8064e72acff660a3c50cba0ff08016079e3
yb3712590 <494888545@qq.com>
2017-11-20 22:05:17 +0800

    ss_python install script

--- install_shadowsocks_python.sh
+++ install_shadowsocks_python.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-yum install -y epel-release
-yum install -y python python-setuptools python-pip 
-pip install  shadowsocks
+#no longer use pip
+#yum install -y epel-release
+#yum install -y python python-setuptools python-pip 
+#pip install  shadowsocks
+
+cd /tmp
+wget --no-check-certificate -O shadowsocks-master.zip https://github.com/shadowsocks/shadowsocks/archive/master.zip
+unzip -q shadowsocks-master.zip
+cd shadowsocks-master
+python setup.py install
+cd /tmp && rm -rf shadowsocks-master/ && rm -f shadowsocks-master.zip
