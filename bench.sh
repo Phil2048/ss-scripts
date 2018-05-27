bd002ec157ba9f7eee098310d210e363bcff3c80
yb3712590 <yb3712590@gmail.com>
2018-05-28 01:50:42 +0800

    0528 bench script

--- bench.sh
+++ bench.sh
@@ -1,3 +1,3 @@
 cd /tmp
-wget -N --no-check-certificate https://raw.githubusercontent.com/FunctionClub/ZBench/master/ZBench-CN.sh && bash ZBench-CN.sh
-rm ZBench-CN.sh
+wget -qO- https://raw.githubusercontent.com/oooldking/script/master/superbench.sh | bash
+rm superbench.sh
