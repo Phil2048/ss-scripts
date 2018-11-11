656a9b991d5cff314c29358c84c3878c350fde08
yb3712590 <yb3712590@gmail.com>
2018-11-11 16:47:09 +0000

    env things

---
+++ install_env.sh
@@ -0,0 +1,7 @@
+yum install -y epel-release
+yum install -y git httpd mariadb-server mariadb php php-mysql
+yum install -y gcc automake autoconf mesa-libEGL-devel libtool make build-essential libtool asciidoc xmlto libffi-devel
+yum install -y curl curl-devel zlib-devel openssl-devel perl perl-devel cpio expat-devel gettext-devel
+yum install -y udns udns-devel libev-devel mbedtls mbedtls-devel libsodium libsodium-devel pcre-devel asciidoc c-ares-devel
+yum install -y unzip python python-devel python-setuptools openssl openssl-devel curl wget unzip gcc automake autoconf make libtool
+yum install -y mercurial git gcc wget psmisc
