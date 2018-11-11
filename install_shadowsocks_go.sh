e84fdbfd528c3d7d2f8f31c954f194d8ad83796e
yb3712590 <yb3712590@gmail.com>
2018-11-11 16:44:51 +0000

    env things

--- install_shadowsocks_go.sh
+++ install_shadowsocks_go.sh
@@ -2,12 +2,13 @@
 yum install -y mercurial git gcc wget psmisc
 mkdir /go
 cd /go
-wget https://storage.googleapis.com/golang/go1.6rc2.linux-amd64.tar.gz
-tar -zxvf go1.6rc2.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz
+tar -zxvf go1.11.1.linux-amd64.tar.gz
 mkdir /gopkg
 export GOROOT=/go/go
 export PATH=$GOROOT/bin:$PATH
 export GOPATH=/gopkg
 source /etc/profile
 go version
-go get github.com/shadowsocks/shadowsocks-go/cmd/shadowsocks-server
+#go get github.com/shadowsocks/shadowsocks-go/cmd/shadowsocks-server
+go get -u -v github.com/shadowsocks/go-shadowsocks2
--- reset_ss.sh
+++ reset_ss.sh
@@ -3,7 +3,7 @@ SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 source $SHELL_FOLDER/ss_config.cfg
 killall ss-server 2>/dev/zero
 killall ssserver 2>/dev/zero
-killall shadowsocks-server 2>/dev/zero
+killall go-shadowsocks2 2>/dev/zero
 mysql -htky.vgpapa.com -u$user -p$password $database -N -e 'update user set active=0 where utype=1 and upload+download>transfer_limit'
 mysql -htky.vgpapa.com -u$user -p$password $database -N -e 'update user set active=0 where utype=0 and curdate()>exp'
 mysql -htky.vgpapa.com -u$user -p$password $database -N -e 'update user set active=1 where utype=1 and upload+download<transfer_limit'
@@ -14,10 +14,10 @@ do
 #echo "nohup /gopkg/bin/shadowsocks-server -p=\"$a\" -k=\"$b\" -m=\"table\" -t=60 >/dev/null & "
 #
 #golang
-#nohup /gopkg/bin/shadowsocks-server -p $a -k $b -m $method -t $timeout > /dev/null 2>&1 & 
+nohup /gopkg/bin/go-shadowsocks2 -s "ss://AEAD_CHACHA20_POLY1305:$b@:$a" > /dev/null 2>&1 & 
 #
 #libev
-nohup ss-server -p $a -k $b -m $method -t $timeout -u $otherparam >/dev/null 2>&1 &
+#nohup ss-server -p $a -k $b -m $method -t $timeout -u $otherparam >/dev/null 2>&1 &
 #
 #python
 #nohup ssserver -p $a -k $b -m $method -t $timeout $otherparam >/dev/null 2>&1 &
