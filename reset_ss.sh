6ed6416fa7c1a860f9ed122ad2a3844f28fbefaf
yb3712590 <yb3712590@gmail.com>
2019-04-05 00:59:52 +0800

    ss go ver.

--- reset_ss.sh
+++ reset_ss.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 source $SHELL_FOLDER/ss_config.cfg
-killall ss-server 2>/dev/zero
-killall ssserver 2>/dev/zero
-killall go-shadowsocks2 2>/dev/zero
+killall ss-server 2>/dev/null
+killall ssserver 2>/dev/null
+killall go-shadowsocks2 2>/dev/null
+killall speederv2_amd64 2>/dev/null
+killall udp2raw_amd64 2>/dev/null
 mysql -htky.vgpapa.com -u$user -p$password $database -N -e 'update user set active=0 where utype=1 and upload+download>transfer_limit'
 mysql -htky.vgpapa.com -u$user -p$password $database -N -e 'update user set active=0 where utype=0 and curdate()>exp'
 mysql -htky.vgpapa.com -u$user -p$password $database -N -e 'update user set active=1 where utype=1 and upload+download<transfer_limit'
@@ -14,10 +16,10 @@ do
 #echo "nohup /gopkg/bin/shadowsocks-server -p=\"$a\" -k=\"$b\" -m=\"table\" -t=60 >/dev/null & "
 #
 #golang
-#nohup /gopkg/bin/go-shadowsocks2 -s "ss://AEAD_CHACHA20_POLY1305:$b@:$a" > /dev/null 2>&1 & 
+nohup /gopkg/bin/go-shadowsocks2 -s "ss://AEAD_CHACHA20_POLY1305:$b@:$a" > /dev/null 2>&1 & 
 #
 #libev
-nohup ss-server -p $a -k $b -m $method -t $timeout -u $otherparam >/dev/null 2>&1 &
+#nohup ss-server -p $a -k $b -m $method -t $timeout -u $otherparam >/dev/null 2>&1 &
 #
 #python
 #nohup ssserver -p $a -k $b -m $method -t $timeout $otherparam >/dev/null 2>&1 &
